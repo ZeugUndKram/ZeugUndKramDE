8be05b82e05222fae3fe188b1410686d21850390
Viktor Dombrowski <47988211+viktor1234678@users.noreply.github.com>
2026-03-30 02:36:34 +0200

    ,

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
+// https://vite.dev/config/
+export default defineConfig({
+  // This ensures the site loads correctly at vikdo.github.io/zeugundkramde/
+  base: '/zeugundkramde/', 
+  plugins: [
+    vue(),
+    vueDevTools(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+})
