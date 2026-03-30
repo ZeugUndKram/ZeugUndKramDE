d0f35c69d44aa354a5107eaec835fed0f80da8b6
Viktor Dombrowski <47988211+viktor1234678@users.noreply.github.com>
2026-03-30 02:40:02 +0200

    s

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 // https://vite.dev/config/
 export default defineConfig({
   // This ensures the site loads correctly at vikdo.github.io/zeugundkramde/
-  base: '/zeugundkramde/', 
+  base: '/', 
   plugins: [
     vue(),
     vueDevTools(),
